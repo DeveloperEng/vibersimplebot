b7dca3f9853c4ff022ca1bf465f7271784761dad
DeveloperEng <90826181+DeveloperEng@users.noreply.github.com>
2021-11-24 13:21:47 +0300

    Add files via upload

--- SimpleViberBot.py
+++ SimpleViberBot.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 
 @app.route('/setWebHook', methods=['GET'])
 def setWebHook():
-    return "Success" + str(request.url)
+    adress = request.url
+    print(adress)
+    return "Success"
 
 
