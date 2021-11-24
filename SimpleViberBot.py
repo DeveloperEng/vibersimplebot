2ce270a2a9c7d424f9b591379cd5068b8bc28d16
DeveloperEng <90826181+DeveloperEng@users.noreply.github.com>
2021-11-24 13:40:04 +0300

    Add files via upload

--- SimpleViberBot.py
+++ SimpleViberBot.py
@@ -4,9 +4,9 @@ from flask import Flask, request, Response
 import json
 
 address_api_itilium = os.environ['AddressApiItilium']
-login_itilium = os.environ['LoginItilium']
-password_itilium = os.environ['PasswordItilium']
-auth_key = os.environ['AuthKey']
+#login_itilium = os.environ['LoginItilium']
+#password_itilium = os.environ['PasswordItilium']
+auth_key = os.environ['AuthToken']
 
 app = Flask(__name__)
 
