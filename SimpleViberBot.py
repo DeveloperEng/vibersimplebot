b41c764aef7bbc1901b6cb1e5251ed47e49330b2
DeveloperEng <90826181+DeveloperEng@users.noreply.github.com>
2021-11-24 16:24:40 +0300

    Add files via upload

--- SimpleViberBot.py
+++ SimpleViberBot.py
@@ -41,7 +41,7 @@ def setWebHook():
 
 @app.route('/', methods=['POST'])
 def incoming():
-    logger.debug("received request. post data: {0}".format(request.get_data()))
+  #  logger.debug("received request. post data: {0}".format(request.get_data()))
     # every viber message is signed, you can verify the signature using this method
     if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
         return Response(status=403)
@@ -59,7 +59,7 @@ def incoming():
         viber.send_messages(viber_request.get_user.id, [
             TextMessage(text="thanks for subscribing!")
         ])
-    elif isinstance(viber_request, ViberFailedRequest):
-        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+  #  elif isinstance(viber_request, ViberFailedRequest):
+   #     logger.warn("client failed receiving message. failure: {0}".format(viber_request))
 
     return Response(status=200)
