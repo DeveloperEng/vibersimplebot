6f3aceedc0b635aaaa74eadcb99dadbe2b0982b0
DeveloperEng <90826181+DeveloperEng@users.noreply.github.com>
2021-11-24 13:13:30 +0300

    Add files via upload

--- SimpleViberBot.py
+++ SimpleViberBot.py
@@ -1,48 +1,13 @@
 import os
-import time
-import random
-import datetime
-from viberbot import Api
-from viberbot.api.bot_configuration import BotConfiguration
-from viberbot.api.messages import VideoMessage
-from viberbot.api.messages.picture_message import PictureMessage
-from viberbot.api.messages.text_message import TextMessage
-from viberbot.api.messages.keyboard_message import KeyboardMessage
-from viberbot.api.messages.rich_media_message import RichMediaMessage
 import requests
-import logging
 from flask import Flask, request, Response
-import psycopg2
-from viberbot.api.viber_requests import ViberConversationStartedRequest
-from viberbot.api.viber_requests import ViberFailedRequest
-from viberbot.api.viber_requests import ViberMessageRequest
-from viberbot.api.viber_requests import ViberSubscribedRequest
-from viberbot.api.viber_requests import ViberUnsubscribedRequest
-from viberbot.api.viber_requests import ViberDeliveredRequest
-
 import json
-
-
-address_api_itilium = os.environ['AddressApiItilium']
-login_itilium = os.environ['LoginItilium']
-password_itilium = os.environ['PasswordItilium']
-auth_token_out = os.environ['AuthToken']
-
-current_thread = {'id': 0}
 
 app = Flask(__name__)
 
-viber = Api(BotConfiguration(
-    name='Itilium-bot',
-    avatar='http://site.com/avatar.jpg',
-    auth_token=auth_token_out
-))
 
-
-
-@app.route('/',  methods=['GET'])
-def IncomingGet():
-            return "Регистрация бота прошла успешно"
-
+@app.route('/setWebHook', methods=['GET'])
+def setWebHook():
+    return "Success"
 
 
