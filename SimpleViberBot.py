824c6d951c7e8e7d10d1d404ae49bcbad55c78d1
DeveloperEng <90826181+DeveloperEng@users.noreply.github.com>
2021-11-24 16:48:05 +0300

    Add files via upload

--- SimpleViberBot.py
+++ SimpleViberBot.py
@@ -31,40 +31,40 @@ viber = Api(BotConfiguration(
 ))
 print("Yes")
 #viber.set_webhook(request.url)
-#@app.route('/', methods=['GET'])
-#def setWebHook():
- #   viber = Api(BotConfiguration(
- #               name='SimpleViberBotItil',
- #               avatar='http://site.com/avatar.jpg',
- #               auth_token=auth_key
- #              ))
+@app.route('/', methods=['GET'])
+def setWebHook():
+    viber = Api(BotConfiguration(
+                name='SimpleViberBotItil',
+                avatar='http://site.com/avatar.jpg',
+                auth_token=auth_key
+               ))
  #  try:
  # viber.set_webhook(request.url)
   # except Exception as e:
    #    return "Failed" + str(e)
-    #return "Success" + str(request.url)  + str(auth_key)
+    return "Success" + str(request.url)  + str(auth_key)
 
-#@app.route('/', methods=['POST'])
-#def incoming():
-#    logger.debug("received request. post data: {0}".format(request.get_data()))
+@app.route('/', methods=['POST'])
+def incoming():
+    logger.debug("received request. post data: {0}".format(request.get_data()))
     # every viber message is signed, you can verify the signature using this method
-#    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
-#        return Response(status=403)
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
 
     # this library supplies a simple way to receive a request object
-#    viber_request = viber.parse_request(request.get_data())
+    viber_request = viber.parse_request(request.get_data())
 
-#    if isinstance(viber_request, ViberMessageRequest):
-#        message = viber_request.message
-#        # lets echo back
-#        viber.send_messages(viber_request.sender.id, [
-#            message
-#        ])
-#   elif isinstance(viber_request, ViberSubscribedRequest):
-#        viber.send_messages(viber_request.get_user.id, [
-#            TextMessage(text="thanks for subscribing!")
-#        ])
-#    elif isinstance(viber_request, ViberFailedRequest):
-#        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        # lets echo back
+        viber.send_messages(viber_request.sender.id, [
+            message
+        ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
+        ])
+    elif isinstance(viber_request, ViberFailedRequest):
+        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
 
-#    return Response(status=200)
+    return Response(status=200)
