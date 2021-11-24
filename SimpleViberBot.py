1f27aabece0aded79c57b589bd3d4fcd385327c7
DeveloperEng <90826181+DeveloperEng@users.noreply.github.com>
2021-11-24 13:41:55 +0300

    Add files via upload

--- SimpleViberBot.py
+++ SimpleViberBot.py
@@ -3,18 +3,18 @@ import requests
 from flask import Flask, request, Response
 import json
 
-address_api_itilium = os.environ['AddressApiItilium']
+#address_api_itilium = os.environ['AddressApiItilium']
 #login_itilium = os.environ['LoginItilium']
 #password_itilium = os.environ['PasswordItilium']
-auth_key = os.environ['AuthToken']
+#auth_key = os.environ['AuthToken']
 
 app = Flask(__name__)
 
-viber = Api(BotConfiguration(
-    name='SimpleViberBot-bot',
-    avatar='http://site.com/avatar.jpg',
-    auth_token=auth_key
-))
+#viber = Api(BotConfiguration(
+#    name='SimpleViberBot-bot',
+#    avatar='http://site.com/avatar.jpg',
+#    auth_token=auth_key
+#))
 
 @app.route('/SetWebHook', methods=['GET'])
 def setWebHook():
