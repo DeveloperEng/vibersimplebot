a59e109ed031135bc023a55289afc45ec480ded7
DeveloperEng <90826181+DeveloperEng@users.noreply.github.com>
2021-11-24 13:55:17 +0300

    Add files via upload

--- SimpleViberBot.py
+++ SimpleViberBot.py
@@ -2,6 +2,19 @@ import os
 import requests
 from flask import Flask, request, Response
 import json
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages import VideoMessage
+from viberbot.api.messages.picture_message import PictureMessage
+from viberbot.api.messages.text_message import TextMessage
+from viberbot.api.messages.keyboard_message import KeyboardMessage
+from viberbot.api.messages.rich_media_message import RichMediaMessage
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
+from viberbot.api.viber_requests import ViberDeliveredRequest
 
 address_api_itilium = os.environ['AddressApiItilium']
 #login_itilium = os.environ['LoginItilium']
