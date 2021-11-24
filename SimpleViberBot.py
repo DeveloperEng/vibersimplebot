fc19826b66a3cc17f32e5f68a1de50f8b46b73b8
DeveloperEng <90826181+DeveloperEng@users.noreply.github.com>
2021-11-24 15:42:26 +0300

    Add files via upload

--- SimpleViberBot.py
+++ SimpleViberBot.py
@@ -37,8 +37,8 @@ def setWebHook():
                 auth_token=auth_key
                ))
  # address = request.url.replace("/SetWebHook", "")
- #  viber.unset_webhook()
-   # try:
+   viber.unset_webhook()
+   #try:
     #    viber.set_webhook(request.url)
     #except Exception as e:
     #    return "Failed" + str(e) + str(e.args[0]) + str(auth_key)
@@ -46,25 +46,4 @@ def setWebHook():
 
 @app.route('/', methods=['POST'])
 def incoming():
-    logger.debug("received request. post data: {0}".format(request.get_data()))
-    # every viber message is signed, you can verify the signature using this method
-    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
-        return Response(status=403)
-
-    # this library supplies a simple way to receive a request object
-    viber_request = viber.parse_request(request.get_data())
-
-    if isinstance(viber_request, ViberMessageRequest):
-        message = viber_request.message
-        # lets echo back
-        viber.send_messages(viber_request.sender.id, [
-            message
-        ])
-    elif isinstance(viber_request, ViberSubscribedRequest):
-        viber.send_messages(viber_request.get_user.id, [
-            TextMessage(text="thanks for subscribing!")
-        ])
-    elif isinstance(viber_request, ViberFailedRequest):
-        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
-
     return Response(status=200)
